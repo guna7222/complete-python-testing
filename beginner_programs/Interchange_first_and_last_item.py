2a9fbd33729e736197db5b1c85d68fdada773a6f
guna7222 <rrgunasekhar@gmail.com>
2025-06-04 20:56:02 +0530

    interchange first and last items in list

--- beginner_programs/Interchange_first_and_last_item.py
+++ beginner_programs/Interchange_first_and_last_item.py
@@ -1,1 +1,26 @@
 # 18. Python program to interchange first and last elements in a list
+
+class InterchangeListElements:
+
+    def __init__(self, list_of_items):
+        self.list_of_items = list_of_items
+
+    def interchange_first_and_last_element(self):
+        data = self.list_of_items
+        first_item = data[0]
+        last_item = data[-1]
+        print(first_item, last_item)
+        data[0] =  last_item
+        data[-1] =  first_item
+        return data
+
+    def message(self):
+        return_data = self.interchange_first_and_last_element()
+        print(return_data)
+        print(f"Interchanged values {return_data}")
+
+
+# object creation
+items = ["python", "go", "docker", "java"]
+creation = InterchangeListElements(items)
+creation.message()
